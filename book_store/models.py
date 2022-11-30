8b7809cd8dfb223bb27d40d40418a6dd7b64e62b
Ilias <anotherthecharacter@gmail.com>
2022-11-30 14:13:46 +0600

    book model in database

---
+++ book_store/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Book(models.Model):
+    category = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    published = models.DateTimeField(auto_now_add=True)
